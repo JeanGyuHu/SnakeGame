bc222fdd3bb0831d3e394c8c31bbb1c20ff0c0f9
JeanGyuHu <gjwlsrb1700@gmail.com>
2019-06-24 10:59:44 +0900

    feat : reshape function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <gl/glut.h>
 #include <iostream>
 
+void display_callback();
+void init();
+
 int main(int argc, char **argv) {
 
 	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_RGB);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
 
 	glutCreateWindow("SNAKEGAME");
 
+	glutDisplayFunc(display_callback);
+
+	init();
+
+	glutMainLoop();
+
 	return 0;
+}
+
+void init() {
+	glClearColor(1.0, 0.0, 0.0, 1.0);
+
+
+}
+
+void display_callback() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSwapBuffers();
 }
